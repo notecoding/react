6729cc67477777e915319c3dced587264dbdf127
notecoding <63888297+notecoding@users.noreply.github.com>
2025-03-20 11:39:10 +0900

    Add files via upload

---
+++ 0320/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Clock from "./chapter_04/Clock";
+// Clock컴포넌트불러오기
+function App() {
+  return (
+    <div>
+      <Clock />
+    </div>
+  );
+}
+export default App;
