80d3099b60eabc7ba9abc2b2e5a356b6adaf02ec
notecoding <63888297+notecoding@users.noreply.github.com>
2025-03-27 13:09:06 +0900

    Add files via upload

---
+++ 0327/component/class/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Greeting from "./Greeting";
+function App() {
+return (
+<div>
+<Greeting name="홍길동" />
+<Greeting name="이순신" />
+</div>
+);
+}
+export default App;
---
+++ 0327/component/class/Greeting.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+class Greeting extends Component {
+render() {
+}
+}
+Greeting.jsx
+return <h1>안녕하세요, {this.props.name}님!</h1>;
+export default Greeting;
