8f723dfe69080cbc0743c61b53fdd647d2fe703d
notecoding <63888297+notecoding@users.noreply.github.com>
2025-03-27 13:23:49 +0900

    Add files via upload

---
+++ 0327/component/propschildren/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from "./Container";
+function App() {
+return (
+<Container>
+<h1>제목</h1>
+<p>이내용은Container 내부에포함됩니다.</p>
+<button>클릭</button>
+</Container>
+);
+}
+export default App;
---
+++ 0327/component/propschildren/Container.js
@@ -0,0 +1,8 @@
+function Container({ children }) {
+    return (
+    <div style={{ border: "2px solid black", padding: "10px", margin: "10px" }}>
+    {children}
+    </div>
+    );
+    }
+    export default Container;
