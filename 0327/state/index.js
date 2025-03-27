ed001f8592e9dfdb03d1835d4fb1adef8a2ee5dd
notecoding <63888297+notecoding@users.noreply.github.com>
2025-03-27 13:34:31 +0900

    Add files via upload

---
+++ 0327/state/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+ import ReactDOM from "react-dom/client";
+ import NotificationList from'./chapter_06/NotificationList';
+ const root = document.getElementById("root");
+ ReactDOM.createRoot(root).render(<NotificationList />);
