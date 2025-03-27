86766b32e9a071f1fed25dd0e025fb5c00420c8a
notecoding <63888297+notecoding@users.noreply.github.com>
2025-03-27 13:11:00 +0900

    Add files via upload

---
+++ 0327/component/function/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Greeting from "./Greeting";
+function App() {
+return (
+<div>
+<Greeting name="홍길동" />
+<Greeting name="이순신" />
+</div>
+);
+}
+
+export default App;
---
+++ 0327/component/function/Greeting.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+ 
+ function Greeting(props) {
+ return <h1>안녕하세요, {props.name}님!</h1>;
+ }
+ export default Greeting;
