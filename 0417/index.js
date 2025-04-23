1fa76d624a05d9b154ee3cd71821423c2a0b3f58
notecoding <63888297+notecoding@users.noreply.github.com>
2025-04-23 19:06:56 +0900

    Add files via upload

---
+++ 0417/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Counter3 from './chapter_07/Counter3';
+import Example from './chapter_07/Example';
+import Counter4 from './chapter_07/Counter4';
+import Timer from './chapter_07/Timer';
+import MouseTracker from './chapter_07/MouseTracker';
+import FetchData from './chapter_07/FetchData';
+import Accommodate from './chapter_07/Accommodate';
+
+const root= document.getElementById("root");
+
+ReactDOM.createRoot(root).render(<Accommodate/>);
