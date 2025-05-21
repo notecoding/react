6944eb9917a21a9835b5af7ca809e2b08472c35b
notecoding <63888297+notecoding@users.noreply.github.com>
2025-05-21 17:06:55 +0900

    Update ex.sql

--- test/ex.sql
+++ test/ex.sql
@@ -1,6 +1,6 @@
 -- ✅ 사용자 (관리자 + 일반 사용자) pw:admin1234
 INSERT INTO users (name, username, password, is_admin) VALUES
-('관리자', 'admin', '$2b$10$XkO1eA5Z88YDnHdRTzVN..CqsayGbHjea5p7WvFVU5ESz1yMfQx1K', TRUE),
+('관리자', 'admin', '$2b$10$XkO1eA5Z88YDnHdRTzVN..CqsayGbHjea5p7WvFVU5ESz1yMfQx1K', TRUE);
 
 -- ✅ 카테고리
 INSERT INTO categories (name, `order`) VALUES
